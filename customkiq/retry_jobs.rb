31200aa9244476187bebb37a36f90cb667465875
Marek Piasecki <mad@maniak.pro>
2016-10-13 16:24:11 +0200

    solve problem of customized rescue actions

---
+++ customkiq/retry_jobs.rb
@@ -0,0 +1,38 @@
+require 'sidekiq/middleware/server/retry_jobs'
+
+module Customkiq
+
+  class RetryJobs < Sidekiq::Middleware::Server::RetryJobs
+
+    ## example usage in Worker:
+    #
+    # sidekiq_options rescue: {
+    #   NetworkError:  { retry: 5, dead: false, delay: 60 },
+    #   NoMethodError: { retry: 0, dead: true }
+    # }
+
+    private
+
+    def attempt_retry(worker, msg, queue, exception)
+      rescue_opts(msg['rescue'], exception) do |opts|
+        msg['retry'] = opts['retry']
+        msg['dead']  = opts['dead']
+      end
+      super
+    end
+
+    def delay_for(worker, count, exception)
+      rescue_opts(worker.sidekiq_options_hash['rescue'], exception) do |opts|
+        opts[:delay]
+      end || super
+    end
+
+    def rescue_opts(rescue_opts, exception)
+      if rescue_opts
+        exception_opts = rescue_opts[e = exception.class.to_s] || rescue_opts[e.to_sym]
+        yield exception_opts if exception_opts
+      end
+    end
+
+  end
+end
